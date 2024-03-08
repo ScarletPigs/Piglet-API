dd28ee7d0d329a51af71cba31b2ba53abc4dde2f
Rasmus Tanggaard <rasmus@tthost.dk>
2024-03-08 22:47:35 +0100

    Added userrole

--- src/models/tag.py
+++
@@ -1,9 +0,0 @@
-from ..services.database import Base
-from sqlalchemy import Column, Integer, String
-
-class Tag(Base):
-    __tablename__ = "Tags"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    role_name = Column(String)
-    role_type = Column(Integer)
-    description = Column(String)
