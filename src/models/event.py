99c117dde21302a466c259f49184d9627a9f3623
Rasmus Tanggaard <rasmus@tthost.dk>
2024-10-11 10:39:53 +0200

    Update api to account for discord moving to just using usernames and no longer having ID's associated with them

--- src/models/event.py
+++ src/models/event.py
@@ -4,12 +4,12 @@ from sqlalchemy import Column, Integer, String, DateTime
 
 class Event(Base):
     __tablename__ = "Event"
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
     description = Column(String)
     eventTypeId = Column(Integer)
-    createdByUserId = Column(Integer)
+    createdByUser = Column(String)
     modsetId = Column(Integer)
     createdAt = Column(DateTime(timezone=True))
     lastModified = Column(DateTime(timezone=True))
