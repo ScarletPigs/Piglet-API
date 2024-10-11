842a0d1d3061ee18b038c4e34fa1e348931508bd
Rasmus Tanggaard <rasmus@tthost.dk>
2024-10-11 17:41:13 +0200

    Cleanup of all python and python dev related stuff
    Left docker stuff but might also remove this

--- run.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-command = "uvicorn main:app --reload"
-subprocess.run(command, shell=True)
--- src/models/setting.py
+++
@@ -1,8 +0,0 @@
-from ..services.database import Base
-from sqlalchemy import Column, Integer, String
-
-class Setting(Base):
-    __tablename__ = "Setting"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    key = Column(String)
-    value = Column(String)
